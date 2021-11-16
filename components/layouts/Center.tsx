e107f38ea2c351ec59353ce0d0aedaa34257136c
Iain Bean <iainb@cogapp.com>
2021-11-16 19:40:45 +0000

    Update Center component to optionally center children

--- components/layouts/Center.tsx
+++ components/layouts/Center.tsx
@@ -1,15 +1,31 @@
+import classNames from 'classnames'
+
 import styles from './Center.module.scss'
 
 interface Props {
   children: React.ReactNode
   maxWidth?: string
+  centerContentsHorizontally?: boolean
+  centerContentsVertically?: boolean
 }
 
-const Center: React.FC<Props> = ({ children, maxWidth }) => {
-  const customProperties = { '--max-width': maxWidth } as React.CSSProperties;
+const Center: React.FC<Props> = ({
+  children,
+  maxWidth,
+  centerContentsHorizontally = false,
+  centerContentsVertically = false,
+}) => {
+  const customProperties = { '--max-width': maxWidth } as React.CSSProperties
 
   return (
-    <div className={styles.center} style={customProperties}>
+    <div
+      className={classNames({
+        [styles.center]: true,
+        [styles.centerContentsHorizontally]: centerContentsHorizontally,
+        [styles.centerContentsVertically]: centerContentsVertically,
+      })}
+      style={customProperties}
+    >
       {children}
     </div>
   )
