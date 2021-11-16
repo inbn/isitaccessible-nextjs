86c8e67d53e42774eae92139715e47d8910d68c5
Iain Bean <iainb@cogapp.com>
2021-11-16 19:34:16 +0000

    Redirect query params to package route

--- next.config.js
+++ next.config.js
@@ -1,3 +1,18 @@
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/package',
+        has: [
+            {
+              type: 'query',
+              key: 'name'
+            }
+        ],
+        destination: '/package/:name',
+        permanent: true,
+      },
+    ]
+  },
 }
