b06062e13803c8780286e86e6e3037a06e1b6c34
Iain Bean <iainb@cogapp.com>
2022-01-10 19:55:19 +0000

    Add social sharing meta tags

---
+++ pages/_document.tsx
@@ -0,0 +1,46 @@
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:site" content="@iainmbean" />
+          <meta name="twitter:title" content="Is it accessible?" />
+          <meta
+            name="twitter:description"
+            content="Check if that npm package you’re about to install has accessibility issues"
+          />
+          <meta
+            name="twitter:image"
+            content={`https://${
+              process.env.VERCEL_URL || 'isitaccessible.dev'
+            }/share-image.png`}
+          />
+          <meta
+            name="twitter:image:alt"
+            content="Is it accessible? Check if that npm package you’re about to install has accessibility issues"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
